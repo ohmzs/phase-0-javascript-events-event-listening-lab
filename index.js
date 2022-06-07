6000ccf7248153de0da2834fb4c9749b311ddca9
ohmzs <38644134+ohmzs@users.noreply.github.com>
2022-06-07 14:05:12 -0400

    commit tree e5c30dec296d16bdcbcda9bd0553a6244db86ed2

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
+// function addingEventListener() {
+// }
+
+
+
+
+
+
+// //select elements
+// const grandparent = document.getElementById('grandparent')
+// const parent = document.querySelector('#parent')
+// const child = document.querySelector('#')
+
+// // addEventListener
+// //grandparent.addEventListener('click', e=> console.log(e.target))
+// parent.addEventListener('click', handleParent)
+// child.addEventListener('mouseover', () => child.style.backgroundColor = 'orange')
+// button.addEventListener('click', e => handleButtonClick(e))
+
+// // document.addEventListener('mouseover',onmousemove)
+
+
+
+
+
+// //callbacks
+// function handleParent(e) {
+//     console.log(e)
+// }
+
+// const handleButtonClick = e => {
+//         //console.log(e)
+//     if(button.textContent === "Press Me"){
+//     e.target.textContent = "Swapped Text!"
+//     } else {
+//         e.target.textContent = "Press Me"
+//     }
+// }
+
+// // onmousemove = function(e){console.log("mouse location:", e.clientX, e.clientY)}//
+
+// //Array.from(nodeList)
+
+
+   
+const input = document.querySelector('input');
+
 function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    return "clicked"
+  });
 }
